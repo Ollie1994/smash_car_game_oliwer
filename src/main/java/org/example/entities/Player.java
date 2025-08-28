0df486b94bbaf28d5d7f01cf6961607181d964a1
SatangSagwa <emilia.ivarsson@live.se>
2025-08-28 10:51:40 +0200

    Addded variables and constructor tp Player class

--- src/main/java/org/example/entities/Player.java
+++ src/main/java/org/example/entities/Player.java
@@ -1,5 +1,8 @@
 package org.example.entities;
 
+import org.example.objects.powerUps.PowerUp;
+import org.example.objects.vehicles.Vehicle;
+
 /**
  * Purpose: Should represent the player character.
  * It should be a composite object that contains the player's health,
@@ -15,5 +18,36 @@ package org.example.entities;
 // TODO: behaviour to the current PowerUp object. The PowerUp object should act as a strategy for handling damage.
 
 public class Player extends GameObject{
+    private int health;
+    private PlayerVehicle vehicle;
+    private PowerUp powerUp;
 
+    public Player(int health, PlayerVehicle vehicle) {
+        this.health = health;
+        this.vehicle = vehicle;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public PlayerVehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(PlayerVehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public PowerUp getPowerUp() {
+        return powerUp;
+    }
+
+    public void setPowerUp(PowerUp powerUp) {
+        this.powerUp = powerUp;
+    }
 }
